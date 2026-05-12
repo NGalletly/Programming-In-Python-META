67421ac6ad337b45eb6196bf624cb05a8a415dbb
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-05-12 10:51:22 +0100

    added template literals

--- Intro-Python/conditionals.py
+++ Intro-Python/conditionals.py
@@ -1,8 +1,11 @@
-bill = 0.1
+bill = 110
+
+discount1 = bill * .9
 
 if bill<=0:
     print("invalid bill!")
 elif bill>100:
-    print(f"bill is {str(bill)}, which is over 100.")
+    print(f"bill is {str(bill)}, which is over 100 so you will receive a 10% discount.\
+ After discount you will be charged {str(discount1)} which is a saving of {str(bill-discount1)}")
 else:
     print(f"bill is {str(bill)}, which is under 100")
