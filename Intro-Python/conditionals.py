d9ab5d4d7088f800ff3e67ef49c17b649baa66d7
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-05-12 11:13:23 +0100

    for loops and while loops

--- Intro-Python/conditionals.py
+++ Intro-Python/conditionals.py
@@ -13,3 +13,32 @@ elif bill>100:
  After discount you will be charged {str(discount1)} which is a saving of {str(bill-discount1)}")
 else:
     print(f"bill is {str(bill)}, which is under 100")
+
+# MATCH statement
+
+https_status = 200
+
+match https_status:
+    case 200 | 201:
+        print("Success!")
+    case 400:
+        print("Not Found!")
+    case 500 | 501:
+        print("Server Error!")
+    case _:
+        print("Unknown status!")
+
+ # FOR LOOPS
+
+favourites = ["cake", "ice cream", "apple pie"]
+
+for i in range(10):
+	print('Looping...' , i)
+
+for item in favourites:
+    print("I like this desert", item)
+
+count = 0
+while count  < len(favourites):
+		print("while I like this desert ", favourites[count])
+		count += 1
