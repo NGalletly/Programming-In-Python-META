90b3c3bef6429259fa858c1ba965b42879f26ad8
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-05-12 09:18:11 +0100

    spiking direct and output formatting

--- Intro-Python/intro.py
+++ Intro-Python/intro.py
@@ -18,3 +18,14 @@ multi line ignore not in green?
 # muti
 # line 
 # ignore
+
+#Direct formatting
+a=10
+b=5
+ans = a+b
+print("Adding the value of {} and {} will equal {}".format(a,b,ans))
+
+
+#Output formatting
+print("I like {0} more than {1}".format("oranges", "grapes"))
+print("I like {1} more than {0}".format("oranges", "grapes"))
