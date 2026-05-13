48aa5060399f208a20b13f762163ff044a8289f6
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-05-13 12:31:47 +0100

    spiking functions

--- Intro-Python/control-flow-assignment.py
+++ Intro-Python/control-flow-assignment.py
@@ -1,10 +1,3 @@
-
-# 6.  Inside the for loop increment the counter by 1.
-
-# 7.  Add a print statement outside the for loop to print the value of the count variable.
-
-# 8.  Finally, add a break statement directly after the print statement inside the if condition for finding the number. 
-
 num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
 
 # 1.   Under the num_list create a new for loop and print out each value on the list in sequential order.
@@ -24,6 +17,30 @@ num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
 # 4.  Update the for loop to use the enumerate function so you can get and use the index. Alter the condition to look for number 36 and print out the following: ‘Number found at position: ‘, index number
 # for i, num in enumerate(num_list):
 #     if num == 36:
-#         print(f"Index: {i}, Number: {num}")
+#         print(‘Number found at position: ‘, index number)
 
 # 5.  Next, create a new variable called count and assign it a value of 0 and place it outside the for loop.
+# 6.  Inside the for loop increment the counter by 1.
+# 7.  Add a print statement outside the for loop to print the value of the count variable.
+# 8.  Finally, add a break statement directly after the print statement inside the if condition for finding the number. 
+# count = 0 
+
+
+# for i, num in enumerate(num_list):
+#     count+=1
+#     if num == 36:
+#         print('Number found at position: ', i)
+#         break
+    
+# print(count)
+
+bill = 175.00
+
+def taxCalc(bill):
+    tax_rate = 15
+    total_tax = (bill * tax_rate) / 100
+    print("Total tax:", total_tax)
+    return "Total tax:", total_tax
+ 
+
+print(taxCalc(bill))
