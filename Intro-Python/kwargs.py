3685e00f23a37dd73ef1d046807460f28c2c0d42
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-05-14 11:27:18 +0100

    args and kwargs lesson

--- Intro-Python/kwargs.py
+++ Intro-Python/kwargs.py
@@ -1,4 +1,5 @@
-#kwargs = key word arguements
+# args = spread arguements
+
 
 def sum_of(a,b):
     return a + b
@@ -15,3 +16,15 @@ def sum_of(*args):
 
 print(sum_of(4,5,6))
 print(sum_of(4,5,6,12,4,2,3,3))
+
+#kwargs = key word arguements
+
+def add_total(**kwargs):
+    sum = 0
+    items = []
+    for key,value in kwargs.items():
+        items.append(key)
+        sum += value
+    return items, sum
+    
+print(add_total(coffee = 2.99, tea = 1.00, cake = 4.50))
