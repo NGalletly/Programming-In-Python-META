7f5fe5671bc0c0ba3ea1257d43c9556e23d439d1
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-05-15 09:26:36 +0100

    completed error handling task

--- Intro-Python/errors.py
+++ Intro-Python/errors.py
@@ -24,16 +24,19 @@
 #     print("Error:", e , ". Please check the index range of list before assigning.")
 
 def divide_by(a, b):
-    return a / b
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return 0
+    except Exception as e:
+        print("Error:", e, ". Cannot divide by zero.")
 
-try:
-    ans = divide_by(40, 0)
-    print(ans)
-except Exception as e:
-    print("Error:", e, ". Cannot divide by zero.")
+
+ans = divide_by(40, 0)
+print(ans)
 
 try:
     with open('file_does_not_exist.txt', 'r') as file:
         print(file.read())
 except Exception as e:
-    print("Error:", e )
+    print("Unable to locate file:", e)
