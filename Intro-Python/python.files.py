58fbc634ab36b9e7beebd54a5e2e6a41929b656e
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-05-15 09:55:33 +0100

    added error handling to new files

---
+++ Intro-Python/python.files.py
@@ -0,0 +1,10 @@
+with open('newfile.txt', 'w') as file:
+    file.write('This is a new file created!')
+
+try:
+    with open('newfile2.txt', 'w') as file2:
+        file2.writelines(['\nThis is a new file created!', '\nThis is line 2', "\nthis is line 3!"])
+except FileNotFoundError as e:
+    print("File not found!", e)
+except Exception as e:
+    pring("Something went wrong:", e)
