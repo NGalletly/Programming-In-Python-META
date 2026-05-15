44a7633a846000f430e8ff1cdebf140a7fbc49b7
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-05-15 09:21:38 +0100

    Added divide by zero and no such file error

--- Intro-Python/errors.py
+++ Intro-Python/errors.py
@@ -15,11 +15,25 @@
 
 # print(divide_by(40,0))
 
-items = [1,2,3,4,5]
+# items = [1,2,3,4,5]
+
+# try:
+#     item = items[6]
+#     print(item)
+# except Exception as e:  
+#     print("Error:", e , ". Please check the index range of list before assigning.")
+
+def divide_by(a, b):
+    return a / b
 
 try:
-    item = items[6]
-    print(item)
-except Exception as e:  
-    print("Error:", e , ". Please check the index range of list before assigning.")
+    ans = divide_by(40, 0)
+    print(ans)
+except Exception as e:
+    print("Error:", e, ". Cannot divide by zero.")
 
+try:
+    with open('file_does_not_exist.txt', 'r') as file:
+        print(file.read())
+except Exception as e:
+    print("Error:", e )
