d3dc5225b22f054a89451906bdb920b68e0cf607
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-05-15 09:17:51 +0100

    added index out of range exception

--- Intro-Python/errors.py
+++ Intro-Python/errors.py
@@ -1,10 +1,25 @@
-def divide_by(a,b): 
-    return a / b
+# def divide_by(a,b): 
+#     return a / b
 
-try:
-    ans = divide_by(40,0)
-except:
-    print("Something went wrong")
+# try:
+#     ans = divide_by(40,0)
+# except Exception as e:
+#     print("Something went wrong!", e)
+#     print(e.__class__)
 
+# try:
+#     ans = divide_by(40,0)
+# except ZeroDivisionError as e:
+#     print(e, ", cannot divide by 0.")
+#     print(e.__class__)
 
 # print(divide_by(40,0))
+
+items = [1,2,3,4,5]
+
+try:
+    item = items[6]
+    print(item)
+except Exception as e:  
+    print("Error:", e , ". Please check the index range of list before assigning.")
+
