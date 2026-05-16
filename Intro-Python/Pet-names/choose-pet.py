82a276bfa3206ed18297bf2ddbe65f3e1e5d692d
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-05-16 10:53:18 +0100

    pet name assignment)

---
+++ Intro-Python/Pet-names/choose-pet.py
@@ -0,0 +1,9 @@
+import random
+
+f = open("petnames.txt", "r")
+f_content = f.read()
+# print(f_content)
+f_content_list = f_content.split("\n")
+# print(f_content_list)
+f.close()
+print(random.choice(f_content_list))
