92d9544fa33a3e898ea8501525796278b8459193
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-05-18 10:10:54 +0100

    coffee exercise

---
+++ Intro-Python/coffee.py
@@ -0,0 +1,18 @@
+"""
+Step 1: Start with the inputs - what is needed to make a cup of instant coffee?
+a list of items : ["kettle", "coffee","milk","sugar", "mug"]
+
+Step 2: Think about all the steps required in the physical aspect of making a cup of instant coffee.
+procedure. 
+1.check if water in kettle, if not, fill up with water. if so turn on.
+2.place coffee in mug
+3.add sugar now if needed
+4.when kettle finishes, add hot water to mug : 230ml
+5.stir and add milk if needed
+
+Step 3: Consider the edge cases of optional things like milk or sugar, some people may want it. 
+Add sugar in step 3 and milk in step 5 if needed.
+
+Step 4: The algorithm when complete should have as its final result a cup of coffee.
+
+"""
