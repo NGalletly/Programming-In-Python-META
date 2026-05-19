e8f19bf7eae6d4048e6c68a090d546320c7d638b
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-05-19 09:54:16 +0100

    map task

--- Intro-Python/functional_programming.py
+++ Intro-Python/functional_programming.py
@@ -1,3 +1,14 @@
 coffees = [ "americano", "long black", "cappucino", "latte"]
 sorted_coffees= sorted(coffees)
-print(sorted_coffees)
+# print(sorted_coffees)
+
+def reverse(str):
+    return str[::-1]
+
+reversed_coffees_map = map(reverse, coffees)
+reversed_coffees_list = []
+
+for x in reversed_coffees_map:
+    reversed_coffees_list.append(x)
+
+print(reversed_coffees_list)
