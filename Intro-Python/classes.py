6fe73ddade4583849a19ea88cb6de86e7208cacc
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-05-21 09:04:08 +0100

    added classes

---
+++ Intro-Python/classes.py
@@ -0,0 +1,12 @@
+class MyClass:
+    a = 5
+
+    def hello(self):
+        print("hello world")
+
+my_class = MyClass()
+print(my_class.a)
+print(my_class.hello())
+
+
+
--- Intro-Python/functional_programming.py
+++ Intro-Python/functional_programming.py
@@ -49,3 +49,9 @@ def find_factorial_recursive(n):
 
 
 
+numbers = [15, 30, 47, 82, 95]
+def lesser(numbers):
+   return numbers < 50
+
+small = list(map(lesser, numbers))
+print(small)
