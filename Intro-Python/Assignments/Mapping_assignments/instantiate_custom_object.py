adfa4bd379e4f84b881634f81ea430083af871af
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-05-21 10:26:24 +0100

    __init__ constructor

--- Intro-Python/Assignments/Mapping_assignments/instantiate_custom_object.py
+++ Intro-Python/Assignments/Mapping_assignments/instantiate_custom_object.py
@@ -23,3 +23,33 @@ print(pizza.items)
 print(pizza.contents())
 print(pasta.items)
 print(pasta.contents())
+
+
+# class MyFirstClass:
+#     print("Who wrote this?")
+#     index = "Author-Book"
+
+#     def hand_list(self, philosopher, book):
+#         print(philosopher + " wrote the book: " + book)
+
+
+# whodunnit = MyFirstClass()
+
+# whodunnit.hand_list("sun - tzu", "art of war")
+
+
+class MyFirstClass:
+    index = "Author-Book"
+
+    def __init__(self):
+        print("Who wrote this?")  # Move the print statement into the constructor
+
+    def hand_list(self, philosopher, book):
+        print(MyFirstClass.index)
+        print(philosopher + " wrote the book: " + book)
+
+
+# Instantiate the class
+whodunnit = MyFirstClass()
+# Call the method with parameters
+# whodunnit.hand_list("Sun Tzu", "The Art of War")
