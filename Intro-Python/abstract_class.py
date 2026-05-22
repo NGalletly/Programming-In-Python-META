9cf7a033aa6bd5280da762713cb983422a44aa07
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-05-22 09:50:02 +0100

    abstract class lesson for donations

--- Intro-Python/abstract_class.py
+++ Intro-Python/abstract_class.py
@@ -5,3 +5,27 @@ class SomeAbstractClass(ABC):
     @abstractmethod
     def someabstractmethod(self):
         pass
+
+
+class Employee(ABC):
+    @abstractmethod
+    def donate(self):
+        pass
+
+
+class Donation(Employee):
+    def donate(self):
+        a = input("How much would you like to donate: ")
+        return a
+
+
+amounts = []
+john = Donation()
+j = john.donate()
+amounts.append(j)
+
+peter = Donation()
+p = peter.donate()
+amounts.append(p)
+
+print(amounts)
