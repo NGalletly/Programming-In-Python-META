1a7f00eda9759d39f87b97984ef82d847d9c9b0f
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-05-25 15:12:32 +0100

    added task

--- Intro-Python/working_with_methods.py
+++ Intro-Python/working_with_methods.py
@@ -1,55 +1,74 @@
+# class A:
+#     def b(self):
+#         return "Function inside A"
+
+
+# class B:
+#     pass
+
+
+# class C:
+#     def b(self):
+#         return "Function inside C"
+
+
+# class D(B, C, A):
+#     pass
+
+
+# class D(C):
+#     pass
+
+
+# d = D()
+# print(d.b())
+
+
+# # function inside C
+
+
+# class A:
+#     def c(self):
+#         return "Function inside A"
+
+
+# class B(A):
+#     def c(self):
+#         return "Function inside B"
+
+
+# class C(A, B):
+#     pass
+
+
+# class D(C):
+#     pass
+
+
+# d = D()
+# # print(d.c())
+
+
+# class A:
+#     pass
+
+
+# class B(A):
+#     pass
+
+
+# class C(B):
+#     pass
+
+
+# c = C()
+# print(c.a())
+
+a = 5
+
+
 class A:
-    def b(self):
-        return "Function inside A"
-
-
-class B:
-    pass
-
-
-class C:
-    def b(self):
-        return "Function inside C"
-
-
-class D(B, C, A):
-    pass
-
-
-class D(C):
-    pass
-
-
-d = D()
-print(d.b())
-
-
-# function inside C
-
-
-class A:
-    def c(self):
-        return "Function inside A"
-
-
-class B(A):
-    def c(self):
-        return "Function inside B"
-
-
-class C(A, B):
-    pass
-
-
-class D(C):
-    pass
-
-
-d = D()
-# print(d.c())
-
-
-class A:
+    a = 7
     pass
 
 
