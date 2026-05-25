b489cdd5dbdc95495012065e181c06cb82311da6
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-05-25 15:01:58 +0100

    added working with methods task

--- Intro-Python/Assignments/Mapping_assignments/method_resolution.py
+++ Intro-Python/Assignments/Mapping_assignments/method_resolution.py
@@ -1,15 +1,63 @@
-class a:
-    num = 5
+# class a:
+#     num = 5
 
 
-class b(a):
-    num = 9
+# class b(a):
+#     num = 9
 
 
-class c(b):
+# class c(b):
+#     pass
+
+
+# print(c.mro())
+# print(c.num)
+# print(help(c))
+
+# ***
+# class A:
+#     def b(self):
+#         return "Function inside A"
+
+
+# class B:
+#     def b(self):
+#         return "Function inside B"
+
+
+# class C(B, A):
+#     # def b(self):
+#     #     return "Function inside C"
+
+#     pass
+
+
+# class D(C):
+#     pass
+
+
+# d = D()
+# print(d.b())
+
+
+class A:
+    def c(self):
+        return "Function inside A"
+
+
+class B:
+    def c(self):
+        return "Function inside B"
+
+
+class C(A, B):
+    def c(self):
+        return "Function inside C"
+
+
+class D(A, C):
     pass
 
 
-print(c.mro())
-print(c.num)
-print(help(c))
+d = D()
+print(d.c)
