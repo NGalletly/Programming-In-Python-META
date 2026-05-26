01170107dc1bb6e367dfa5c554e7e8b1f80c5ece
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-05-26 09:15:35 +0100

    importing task

---
+++ Intro-Python/imports.py
@@ -0,0 +1,15 @@
+# import math
+# from math import sqrt
+# import math as m
+# from math import sqrt as sr
+
+# alias
+# from math import factorial, log10, sqrt
+from math import *
+
+# root = math.sqrt(9)
+root = sqrt(9)
+# root = m.sqrt(9)
+# root = sr(9)
+
+print(root)
